59d5082a107025fb8463f56be5799479de852bab
江涛 <1325746197@qqcom>
2018-05-26 14:00:08 +0800

    项目第一次提交代码

---
+++ empdemo/src/main/java/com/neuedu/mapper/DeptMapper.java
@@ -0,0 +1,22 @@
+package com.neuedu.mapper;
+
+import com.neuedu.entity.Dept;
+
+import java.util.List;
+
+public interface DeptMapper {
+
+    /**
+     * 查村所有dept
+     * @return
+     */
+    List<Dept> listDept();
+
+    /**
+     * 根据id的数组来删除多个dept
+     * @param ids
+     * @return 影响行数
+     */
+    int deleteDeptByIds(int[] ids);
+
+}
